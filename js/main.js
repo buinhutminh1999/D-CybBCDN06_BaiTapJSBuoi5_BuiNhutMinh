ab28cf4b277fe3d48d1e855db8470561f4bfd113
buinhutminh1999 <buinhutminh1999@gmail.com>
2022-12-01 22:54:53 +0700

    fix loi dom

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ function getMyEle(id){
 }
 
 getMyEle('#btnTinh').onclick = function() {
-    var diemChuan =  Number(getmyEle('#inputDiemChuan').value);
-    var khuVuc =  Number(getmyEle('#khuVuc').value);
-    var doiTuong =  Number(getmyEle('#doiTuong').value);
-    var inputDiemmon1 =  Number(getmyEle('#inputDiemmon1').value);
-    var inputDiemmon2 =  Number(getmyEle('#inputDiemmon2').value);
-    var inputDiemmon3 =  Number(getmyEle('#inputDiemmon3').value);
+    var diemChuan =  Number(getMyEle('#inputDiemChuan').value);
+    var khuVuc =  Number(getMyEle('#khuVuc').value);
+    var doiTuong =  Number(getMyEle('#doiTuong').value);
+    var inputDiemmon1 =  Number(getMyEle('#inputDiemmon1').value);
+    var inputDiemmon2 =  Number(getMyEle('#inputDiemmon2').value);
+    var inputDiemmon3 =  Number(getMyEle('#inputDiemmon3').value);
     var diemTong = inputDiemmon1 + inputDiemmon2 + inputDiemmon3 + khuVuc + doiTuong ;
     var thongBaoDiem = ''
     if(diemTong >= diemChuan){
@@ -17,7 +17,7 @@ getMyEle('#btnTinh').onclick = function() {
      thongBaoDiem = 'Bạn đã rớt. ' + diemTong;
     }
  
-    document.querySelector('#txtThongBao').innerHTML = thongBaoDiem
+   getMyEle('#txtThongBao').innerHTML = thongBaoDiem
  }
 
  const TU_0_50 = 500;
@@ -25,12 +25,12 @@ getMyEle('#btnTinh').onclick = function() {
  const TU_101_200 = 850;
  const TU_201_350 = 1100;
  const TU_351_TROLEN = 1300;
- document.querySelector('#btnTienDien').onclick = function() {
-    var inputHoTen =  getmyEle('#inputHoTen').value;
-    var inputSoKw =  Number(getmyEle('#inputSoKw').value);
+getMyEle('#btnTienDien').onclick = function() {
+    var inputHoTen =  getMyEle('#inputHoTen').value;
+    var inputSoKw =  Number(getMyEle('#inputSoKw').value);
     var tienDien = tinhTienDien(inputSoKw,TU_0_50,TU_51_100,TU_101_200,TU_201_350,TU_351_TROLEN)
 
-    document.querySelector('#txtThongBaoTienDien').innerHTML = 'Họ tên: ' + inputHoTen + ' Tiền điện: ' + tienDien
+   getMyEle('#txtThongBaoTienDien').innerHTML = 'Họ tên: ' + inputHoTen + ' Tiền điện: ' + tienDien
  }
  
 function tinhTienDien(inputSoKw,TU_0_50,TU_51_100,TU_101_200,TU_201_350,TU_351_TROLEN){
@@ -56,7 +56,8 @@ getMyEle('#btnThue').onclick = function(){
     var tongThuNhapNam = Number(getMyEle('#tongThuNhapNam').value);
     var soNguoiPhuThuoc = Number(getMyEle('#soNguoiPhuThuoc').value);
     var thuNhapThue = thuNhapChiuThue(tongThuNhapNam,soNguoiPhuThuoc)
-    document.querySelector('#txtThueThuNhap').innerHTML = thuNhapThue
+    console.log(thuNhapThue)
+   getMyEle('#txtThueThuNhap').innerHTML = thuNhapThue
 }
 
 function thuNhapChiuThue(tongThuNhapNam,soNguoiPhuThuoc) {
@@ -98,10 +99,11 @@ getMyEle('#btnTienCap').onclick = function(){
               }else{
                 tongTien = 15 + 75 + 5 * inputSoKetNoi + 50 * soKenhCaoCap;
               }
+              break;
         default:
             alert('Hãy chọn loại khách hàng')
             break;
     }
-    document.querySelector('#txtTienCap').innerHTML = 'Mã khách hàng: ' + maKH + ' Tiền cáp: $' + tongTien
+   getMyEle('#txtTienCap').innerHTML = 'Mã khách hàng: ' + maKH + ' Tiền cáp: $' + tongTien
 }
 
